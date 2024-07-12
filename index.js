6cac747abf3a452ab904658ac91fcb7a864f6dc7
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2024-07-12 11:28:03 +0530

    Added CORS and Nodemon Support

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
+const cors = require('cors'); // Import CORS
 
 const app = express();
 const PORT = process.env.PORT || 9000;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
+
+// Middleware to enable CORS
+app.use(cors());
 
 // Serve static files from the assets folder
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
